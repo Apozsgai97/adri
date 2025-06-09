ee576b4b0d40218269a30cc45ed195be3bdba65c
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2025-06-09 15:40:39 +0200

    Refactor HomeScreen to use ScrollView and add MealCard component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,35 @@
 import { Separator } from "@/components/Separator";
+import { MealCard } from "@/features/meal-plan/components/MealCard";
 import { NutritionCard } from "@/features/meal-plan/components/NutritionCard";
 import { WelcomeCard } from "@/features/meal-plan/components/WelcomeCard";
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView className="flex-1 bg-background">
-      <View className="flex-1 bg-orange-100">
+      <ScrollView
+        contentContainerStyle={{
+          paddingBottom: 40,
+        }}
+        className=" bg-orange-100"
+      >
         <WelcomeCard />
         <Separator />
         <View className="m-6 gap-4">
           <Text className="text-lg font-bold">Your Meal Plan</Text>
           <NutritionCard />
+          <Text className="text-lg font-bold">{"Today's Meals"}</Text>
+          <View className="flex-row flex-wrap justify-between">
+            <MealCard />
+            <MealCard />
+            <MealCard />
+            <MealCard />
+            <MealCard />
+            <MealCard />
+          </View>
         </View>
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
---
+++ features/meal-plan/components/MealCard.tsx
@@ -0,0 +1,31 @@
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/Card";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Image } from "expo-image";
+import { Text } from "react-native";
+
+export function MealCard() {
+  return (
+    <Card className="mb-4 gap-1 items-end">
+      <CardHeader className="items-center pb-0">
+        <Image
+          style={{ width: 130, height: 130 }}
+          source={{
+            uri: "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=600,545",
+          }}
+        />
+        <CardTitle className="">
+          <Text className="text-lg font-bold">Meal Plan</Text>
+        </CardTitle>
+        <CardDescription className="">
+          <Text className="text-lg font-bold">Breakfast</Text>
+        </CardDescription>
+      </CardHeader>
+      <Ionicons name="heart-outline" size={24} />
+    </Card>
+  );
+}
