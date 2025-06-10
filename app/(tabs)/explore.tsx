e1a819154a827ffe1d25fabac2725bf836cdf141
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-06-10 15:09:27 +0200

    change background for login page
    Co-authored-by: Carolinne <carolinnepmelo@gmail.com>

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -5,7 +5,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function LoginScreen() {
   return (
     <SafeAreaView className="flex-1 bg-primary justify-end">
-      <View className="bg-secondary py-16 px-4">
+      <View className="bg-background py-16 px-4">
         <Text className="text-4xl text-center">
           Making Meal Planning Effortless and Enjoyable
         </Text>
@@ -13,11 +13,11 @@ export default function LoginScreen() {
           Discover a world of delicious recipes, personalized meal plans, and
           nutritional insights. Join us in transforming your cooking experience!
         </Text>
-        <Button className="rounded-full mt-4" size={"lg"}>
-          <Text className="text-xl text-background">Log in</Text>
+        <Button className="rounded-full mt-4 border-primary" size={"lg"} variant={"outline"}>
+          <Text className="text-xl text-primary">Log in</Text>
         </Button>
-        <Button variant={"outline"} size={"lg"} className="rounded-full mt-4">
-          <Text className="text-xl">Get started</Text>
+        <Button size={"lg"} className="rounded-full mt-4">
+          <Text className="text-xl text-background">Get started</Text>
         </Button>
       </View>
     </SafeAreaView>
